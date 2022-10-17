94653211e424bb980506bd4934b3fc5bd970961d
emptysky2022 <emptysky2022@gmail.com>
2022-10-17 14:27:14 +0900

    Access Modifier study

---
+++ chap06/src/sec06/Two.java
@@ -0,0 +1,12 @@
+package sec06;
+
+public class Two {
+	One o = new One();
+
+	void print() {
+//		System.out.println(o.secret);
+		System.out.println(o.roommate);
+		System.out.println(o.child);
+		System.out.println(o.anybody);
+	}
+}
