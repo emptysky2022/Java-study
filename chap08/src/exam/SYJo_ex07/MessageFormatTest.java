313edabec0dbe17f11427dd6431ed98abf477c1b
emptysky2022 <emptysky2022@gmail.com>
2022-10-28 15:42:33 +0900

    MessageFormat change

--- chap08/src/exam/SYJo_ex07/MessageFormatTest.java
+++ chap08/src/exam/SYJo_ex07/MessageFormatTest.java
@@ -3,13 +3,13 @@ package exam.SYJo_ex07;
 import java.text.MessageFormat;
 
 public class MessageFormatTest {
-	public static void main(String[] args) {
-		Object[][] data = { { "세종대왕", 1, "조선" }, { "오바마", 2, "미국" }, { "징기스칸", 3, "몽고" } };
+    public static void main(String[] args) {
+        Object[][] data = { { "세종대왕", 1, "조선" }, { "오바마", 2, "미국" }, { "징기스칸", 3, "몽고" } };
 
-		MessageFormat mf = new MessageFormat("이름 : {0}\t\t번호 : {1}\t\t국적 : {2}");
-
-		for (Object[] o : data) {
-			System.out.println(mf.format(o));
-		}
-	}
+        MessageFormat mf = new MessageFormat("{1}\t{0}\t\t{2}");
+        System.out.println("번호\t이름\t\t국적\t\n------------------------------");
+        for (Object[] o : data) {
+            System.out.println(mf.format(o));
+        }
+    }
 }
